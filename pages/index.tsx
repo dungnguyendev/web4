349ed7892114688345d693a60b9b6f48dacd0ea4
dungnguyendev <dungnguyentien.dev@gmail.com>
2023-10-26 20:04:05 +0700

    get donate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,79 @@
-import { ConnectWallet, useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
+import {
+  ConnectWallet,
+  useAddress,
+  useContract,
+  useContractRead,
+} from "@thirdweb-dev/react";
 import { NextPage } from "next";
 import { useEffect, useState } from "react";
-import Header from "./components/layouts/Header";
 
 const Home: NextPage = () => {
-  const [isMobile, setIsMobile] = useState(false)
+  const [isMobile, setIsMobile] = useState(false);
 
-  const address = useAddress()
-  const contractAddress = '0xC74E4380380235380f218f8783e27F5aA0A5428f'
+  const address = useAddress();
+  const contractAddress = "0xC74E4380380235380f218f8783e27F5aA0A5428f";
 
   const { contract } = useContract(contractAddress);
 
-  const { data: totalArt, isLoading: loadingTotalArts } = useContractRead(contract, 'getCampaigns')
-  console.log(totalArt);
+  const { data: getProjects, isLoading: loadingGetProjects } = useContractRead(
+    contract,
+    "getCampaigns"
+  );
 
   const handleCheckMobile = () => {
     if (window.innerWidth < 768) {
-      setIsMobile(true)
+      setIsMobile(true);
     } else {
-      setIsMobile(false)
+      setIsMobile(false);
     }
-  }
+  };
 
   useEffect(() => {
-    handleCheckMobile()
-    window.addEventListener('resize', handleCheckMobile)
+    handleCheckMobile();
+    window.addEventListener("resize", handleCheckMobile);
     return () => {
-      window.removeEventListener('resize', handleCheckMobile)
-    }
-  }, [])
+      window.removeEventListener("resize", handleCheckMobile);
+    };
+  }, []);
 
   if (!isMobile) {
-    return (
-      <div className="">Chúng tôi không hỗ trợ màn hình này</div>
-    )
+    return <div className="">Chúng tôi không hỗ trợ màn hình này</div>;
   }
 
   return (
-    <div className="bg-red-400">
-      <Header />
+    <div>
+      <div>
+        <ConnectWallet
+          dropdownPosition={{
+            side: "bottom",
+            align: "center",
+          }}
+        />
+        <span className="bg-yellow-300">
+          1
+          {address ? (
+            <div>
+              <h1>Tất cả dự án</h1>
+              <div>
+                {loadingGetProjects
+                  ? "Loading"
+                  : getProjects.map((item: any, i: number) => {
+                    if (i === 0) return null;
+                    console.log(item[9]);
+                      
+                      return (
+                        <div key={i}>
+                          <img src={item[9]} alt="" />
+                        </div>
+                      );
+                    })}
+              </div>
+            </div>
+          ) : (
+            "Vui lòng đăng nhập"
+          )}
+        </span>
+      </div>
     </div>
   );
 };
