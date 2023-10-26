4298105f2977c177e5b8709c894f83ab22427c69
dungnguyendev <dungnguyentien.dev@gmail.com>
2023-10-26 18:01:19 +0700

    update part 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,108 +1,54 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import styles from "../styles/Home.module.css";
-import Image from "next/image";
+import { ConnectWallet, useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  const address = useAddress()
+  const contractAddress = '0xE7a589c0e601c87391C5ffEE33d74b8Be6D6CEA6'
+
+  const { contract } = useContract(contractAddress);
+
+  const { data: totalArt, isLoading: loadingTotalArts } = useContractRead(contract, 'getCampaigns')
+
+  const handleCheckMobile = () => {
+    if (window.innerWidth < 768) {
+      setIsMobile(true)
+    } else {
+      setIsMobile(false)
+    }
+  }
+
+  useEffect(() => {
+    handleCheckMobile()
+    window.addEventListener('resize', handleCheckMobile)
+    return () => {
+      window.removeEventListener('resize', handleCheckMobile)
+    }
+  }, [])
+
+  if (!isMobile) {
+    return (
+      <div className="">Chúng tôi không hỗ trợ màn hình này</div>
+    )
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <h1 className={styles.title}>
-            Welcome to{" "}
-            <span className={styles.gradientText0}>
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
-
-          <p className={styles.description}>
-            Get started by configuring your desired network in{" "}
-            <code className={styles.code}>src/index.js</code>, then modify the{" "}
-            <code className={styles.code}>src/App.js</code> file!
-          </p>
-
-          <div className={styles.connect}>
-            <ConnectWallet
-              dropdownPosition={{
-                side: "bottom",
-                align: "center",
-              }}
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://portal.thirdweb.com/"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText1}>Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/dashboard"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText2}>Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText3}>Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
-        </div>
+    <div>
+      <div className="">DUng</div>
+      <div >
+        <ConnectWallet
+          dropdownPosition={{
+            side: "bottom",
+            align: "center",
+          }}
+        />
+        <span className="text-[0.9rem] font-bold text-red-600">
+          {totalArt?.toString()} 🎨
+        </span>
       </div>
-    </main>
+    </div>
   );
 };
 
